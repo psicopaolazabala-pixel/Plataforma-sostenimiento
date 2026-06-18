907d38f60f54c6dc7e4fa018981babf643b9f057
psicopaolazabala-pixel <psicopaola.zabala@gmail.com>
2026-06-18 17:25:57 -0500

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
-import { signup, login } from '../controllers/auth.controller.js';
+import { signup, login, recoverPassword, updatePassword,  } from '../controllers/auth.controller.js';
 
 const router = Router();
 router.post('/signup', signup);
 router.post('/login', login);
+// Añade esta ruta a tus endpoints públicos de Auth
+router.post('/recover-password', recoverPassword);
+router.post('/update-password', updatePassword);
 
 export default router;
