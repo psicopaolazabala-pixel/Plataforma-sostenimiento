3e162c55800ea3e8957f047d6f078b82b4d55bae
psicopaolazabala-pixel <psicopaola.zabala@gmail.com>
2026-07-03 23:14:49 -0500

    Update admin.routes.js

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { checkAuth } from '../middlewares/auth.middleware.js';
 import { requireRole } from '../middlewares/role.middleware.js';
-import { listApplications, reviewDocument, updateApplicationStatus, getAuditHistory, getApplicationDocuments, getDashboardStats, createNewAdmin, deleteDocument, } from '../controllers/admin.controller.js';
+import { listApplications, reviewDocument, updateApplicationStatus, getAuditHistory, getApplicationDocuments, getDashboardStats, createNewAdmin, deleteDocument, getConvocatoriaEstado, toggleConvocatoria} from '../controllers/admin.controller.js';
 
 const router = Router();
 
 router.use(checkAuth);
+
+router.get('/convocatoria-estado', getConvocatoriaEstado);
 router.use(requireRole('ADMINISTRADOR'));
 
 router.get('/applications', listApplications);
@@ -16,5 +18,8 @@ router.get('/application/:solicitudId/documents', getApplicationDocuments);
 router.get('/dashboard/stats', getDashboardStats);
 router.post('/create-admin', createNewAdmin);
 router.delete('/document/:id', deleteDocument);
+// 🚀 RUTAS DEL INTERRUPTOR DE CONVOCATORIAS
+//router.get('/convocatoria-estado', getConvocatoriaEstado);
+router.post('/convocatoria-toggle', toggleConvocatoria);
 
 export default router;
